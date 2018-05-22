487d7a909412937ab02ba2e51c70d49e4a886d3b
Tom <theoreticalatom@gmail.com>
2018-05-22 20:33:58 +0100

    UnityFile
    Initial commit of project.
    Containg
    - Basic Player Controll
    - Basic Camera Controll
    - Pause Functionality
    - Test models
    - Pro Builder,ProGrids, TextMeshPro added

---
+++ FinalProject/Assets/Scripts/Pause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour 
+{
+	private bool paused = false;
+
+	void Start () 
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+        paused = false;
+	}
+
+	void Update () //TODO: Lock camera rotation while paused
+	{
+		if(Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+            if (paused)
+            {
+                Debug.Log("GamePaused");
+                Time.timeScale = 0;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            if (!paused)
+            {
+                Debug.Log("GameUNPaused");
+                Time.timeScale = 1;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+		}
+	}		
+}
---
+++ FinalProject/Assets/Scripts/Player,Camera/PlayerFollow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+    public bool LookAtPlayer = false;
+    public bool RotateAroundPlayer = true;
+    public bool RotateButton = true;
+    public float RotationsSpeed = 5.0f;
+
+
+
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    private bool IsRotateActive
+    {
+        get
+        {
+            if (!RotateAroundPlayer)
+                return false;
+
+            if (!RotateButton)
+                return true;
+
+            if (RotateButton && Input.GetMouseButton(1))
+                return true;
+
+            return false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (IsRotateActive)
+        {
+            Quaternion camTurnAngle =
+                Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationsSpeed, Vector3.up);
+
+            _cameraOffset = camTurnAngle * _cameraOffset;
+        }
+
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        if (LookAtPlayer || RotateAroundPlayer)
+            transform.LookAt(PlayerTransform);
+    }
+}
+
