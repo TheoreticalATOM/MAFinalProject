eeef419efc7f729455612080a99b2dbe3a9a456d
Tom <theoreticalatom@gmail.com>
2018-05-23 20:52:55 +0100

    World Assets / IsNight
    Added some basic world art assets to the project and created a IsNight Bool.
    Currently a lampost turns on and off based of if it is night.

---
+++ FinalProject/Assets/Scripts/Lampost.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lampost : MonoBehaviour //If night turn light on
+{
+	public SystemManager NightCheck;
+	public bool lightOn;
+
+	public GameObject Lamplight;
+
+	void Start () 
+	{
+		NightCheck = GameObject.Find("Manager").GetComponent<SystemManager>();
+	}
+	
+
+	void Update () 
+	{
+		if(NightCheck.isNight == true)
+		{
+			lightOn = true;
+		}
+
+		if(NightCheck.isNight == false)
+		{
+			lightOn = false;
+		}
+
+		if(lightOn == true)
+		{
+		Lamplight.SetActive(true);
+
+		}
+
+		if(lightOn == false)
+		{
+		Lamplight.SetActive(false);
+		}
+	}
+}
--- FinalProject/Assets/Scripts/SystemManager.cs
+++ FinalProject/Assets/Scripts/SystemManager.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class SystemManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public bool isNight;
+
+	void Start () 
+	{
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		if(Input.GetKeyDown("n")) //NOTE: PLACEHOLDER CODE TO TOGGLE BETWEEN NIGHT AND DAY
+		{
+			isNight = !isNight;
+		}
 	}
 }
