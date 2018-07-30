b40e31a8beabd6eb0cedef61eceaece2ec70b019
Tom <theoreticalatom@gmail.com>
2018-07-30 13:31:15 +0100

    Added Lore/BackStory to Help Screen
    Updated Help screen to have
    - Introduction
    - Ship Info
    - Nature Info
    - Wildlife Info
    - Calandar Info

--- FinalProject/Assets/Scripts/HelpScreen.cs
+++ FinalProject/Assets/Scripts/HelpScreen.cs
@@ -7,12 +7,22 @@ public class HelpScreen : MonoBehaviour {
 
 	public GameObject LorePanel;
 	public GameObject ControllPanel;
+	public GameObject ShipInfoPanel;
+	public GameObject resourcesPanel;
+	public GameObject naturePanel;
+	public GameObject wildlifePanel;
+	public GameObject CalanderPanel;
 
 
 	void Start () 
 	{
 		LorePanel.SetActive(false);
 		ControllPanel.SetActive(false);
+		ShipInfoPanel.SetActive(false);
+		resourcesPanel.SetActive(false);
+		naturePanel.SetActive(false);
+		wildlifePanel.SetActive(false);
+		CalanderPanel.SetActive(false);
 	}
 	
 
@@ -25,13 +35,74 @@ public class HelpScreen : MonoBehaviour {
 	{
 		LorePanel.SetActive(true);
 		ControllPanel.SetActive(false);
+		ShipInfoPanel.SetActive(false);
+		resourcesPanel.SetActive(false);
+		naturePanel.SetActive(false);
+		wildlifePanel.SetActive(false);
+		CalanderPanel.SetActive(false);
 	}
 
 	public void ControlTog()
 	{
 		LorePanel.SetActive(false);
 		ControllPanel.SetActive(true);
+		ShipInfoPanel.SetActive(false);
+		resourcesPanel.SetActive(false);
+		naturePanel.SetActive(false);
+		wildlifePanel.SetActive(false);
+		CalanderPanel.SetActive(false);
 	}
+		public void ShipTog()
+	{
+		LorePanel.SetActive(false);
+		ControllPanel.SetActive(false);
+		ShipInfoPanel.SetActive(true);
+		resourcesPanel.SetActive(false);
+		naturePanel.SetActive(false);
+		wildlifePanel.SetActive(false);
+		CalanderPanel.SetActive(false);
+	}
+		public void resourceTog()
+	{
+		LorePanel.SetActive(false);
+		ControllPanel.SetActive(false);
+		ShipInfoPanel.SetActive(false);
+		resourcesPanel.SetActive(true);
+		naturePanel.SetActive(false);
+		wildlifePanel.SetActive(false);
+		CalanderPanel.SetActive(false);
+	}
+		public void natureTog()
+	{
+		LorePanel.SetActive(false);
+		ControllPanel.SetActive(false);
+		ShipInfoPanel.SetActive(false);
+		resourcesPanel.SetActive(false);
+		naturePanel.SetActive(true);
+		wildlifePanel.SetActive(false);
+		CalanderPanel.SetActive(false);
+	}
+		public void WildlifeTog()
+	{
+		LorePanel.SetActive(false);
+		ControllPanel.SetActive(false);
+		ShipInfoPanel.SetActive(false);
+		resourcesPanel.SetActive(false);
+		naturePanel.SetActive(false);
+		wildlifePanel.SetActive(true);
+		CalanderPanel.SetActive(false);
+	}
+		public void CalandarTog()
+	{
+		LorePanel.SetActive(false);
+		ControllPanel.SetActive(false);
+		ShipInfoPanel.SetActive(false);
+		resourcesPanel.SetActive(false);
+		naturePanel.SetActive(false);
+		wildlifePanel.SetActive(false);
+		CalanderPanel.SetActive(true);
+	}
+
 
 
 }
